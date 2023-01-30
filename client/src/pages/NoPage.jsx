96749bb377d1c4e413926f60d63241a409382b50
Thorben <thorbenegberink@gmail.com>
2023-01-30 09:25:40 +0000

    added error page

--- client/src/pages/NoPage.jsx
+++ client/src/pages/NoPage.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const NoPage = (props) => {
   return (
     <div>
-        <p>nopage</p>;
+        <p>This page doesn't exist, please go back to the <a href="http://blog-api-client-rho.vercel.app">homepage</a></p>
     </div>
   )
 };
